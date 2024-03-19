e14bb49eac60d0de241dc120c8c2addf86ef365a
aubreyrui <157694302+aubreyrui@users.noreply.github.com>
2024-03-19 12:02:59 +0800

    commits for 03.19.24

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, ProductType
+
+# Register your models here.
+
+class ProductTypeInLine(admin.TabularInline):
+    model = ProductType
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    inlines = [ProductTypeInLine,]
+
+admin.site.register(Product, ProductAdmin)
+
 
 # Register your models here.
