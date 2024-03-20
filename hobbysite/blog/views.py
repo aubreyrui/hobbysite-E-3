988714202dcbe90f8043147502cb3fce35ecb540
kristine-banzon <kristine.banzon@student.ateneo.edu>
2024-03-20 21:58:59 +0800

    changes in filenames

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -7,9 +7,9 @@ from .models import Article
 
 class ArticleListView(ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'blog_article_list.html'
 
 
 class ArticleDetailView(DetailView):
     model = Article
-    template_name = 'article_detail.html'
+    template_name = 'blog_article_detail.html'
