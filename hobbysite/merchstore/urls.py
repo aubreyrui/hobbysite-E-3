da1250a0ffed43f2acd6267630e3c6669e32d376
kristine-banzon <kristine.banzon@student.ateneo.edu>
2024-03-20 21:22:15 +0800

    blog app by Kristine

--- hobbysite/merchstore/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductDetailView, ProductListView
-
-urlpatterns = [
-    path('items', ProductListView.as_view(), name="product_list"),
-    path('item/<int:pk>', ProductDetailView.as_view(), name="product_detail"),
-]
-
-app_name = "merchstore"
