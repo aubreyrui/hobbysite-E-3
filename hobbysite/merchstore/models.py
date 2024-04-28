bfb06c6be83b5ba1dd4c52c611302ec8a42a2041
aubreyrui <157694302+aubreyrui@users.noreply.github.com>
2024-04-28 13:17:10 +0800

    intial addition of new specs @ merchstore.Product

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
 class ProductType(models.Model):
     name = models.CharField(max_length = 255)
@@ -16,14 +17,34 @@ class Product(models.Model):
     description = models.TextField()
     producttype = models.ForeignKey(ProductType, null=True, on_delete=models.SET_NULL, related_name = 'products')
     price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    stock = models.IntegerField(
+         validators = [
+            MinLengthValidator(0, "Stock value must be positive!")
+        ]
+    )
+    status = models.CharField()
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('merchstore:product_detail', args=str(self.pk))
-
+    
+    def getStatus(self):
+         if self.stock > 0:
+              return "On Sale"
+         else:
+              return "Out of Stock"
+         
     class meta:
         ordering = ['name']
+""" 
+Stock - whole number
+Status - character field with the following options:
+Available (This should be the default value)
+On sale
+Out of stock (when stock is 0)
+
+ """
     
 # Create your models here.
