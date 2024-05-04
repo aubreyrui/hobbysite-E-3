754f7a9ebb484d2e9147c0c3e7fa0f09cef4d780
aubreyrui <157694302+aubreyrui@users.noreply.github.com>
2024-05-04 10:46:57 +0800

    Revert "added new templates, models, urls, views for merchstore"
    This reverts commit a2f6cff631101fca50389614fc17f65bfa657774.

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.core.validators import MinLengthValidator
 
 class ProductType(models.Model):
     name = models.CharField(max_length = 255)
@@ -17,26 +16,13 @@ class Product(models.Model):
     description = models.TextField()
     producttype = models.ForeignKey(ProductType, null=True, on_delete=models.SET_NULL, related_name = 'products')
     price = models.DecimalField(max_digits = 10, decimal_places = 2)
-    stock = models.IntegerField(
-         validators = [
-            MinLengthValidator(0, "Stock value must be nonnegative!")
-        ]
-    )
-
-    status = models.CharField()
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('merchstore:product_detail', args=str(self.pk))
-    
-    def getStatus(self):
-         if self.stock > 0:
-              return "On Sale"
-         else:
-              return "Out of Stock"
-         
+
     class meta:
         ordering = ['name']
 
--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,25 +1,9 @@
 from django.urls import path
-from .views import (
-    ProductDetailView, ProductListView, ProductCartList, ProductCreateView, ProductTransactionList, ProductUpdateView
-)
+from .views import ProductDetailView, ProductListView
 
 urlpatterns = [
-    path('/items', ProductListView.as_view(), name="product_list"),
-    path('/item/<int:pk>', ProductDetailView.as_view(), name="product_detail"),
-    path('/item/add', ProductCreateView.as_view(), name="product_create"),
-    path('/item/<int:pk>.edit',ProductUpdateView.as_view(), name="product_update"),
-    path('/transactions', ProductTransactionList.as_view(), name="product_transaction"),
-    path('/cart', ProductCartList.as_view(), name ="product_cart")
+    path('merchstore/items', ProductListView.as_view(), name="product_list"),
+    path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name="product_detail"),
 ]
 
 app_name = "merchstore"
-
-"""
-Product List View: /merchstore/items
-Product Detail View: /merchstore/item/1
-Product Create View : /merchstore/item/add
-Product Update View: /merchstore/item/1/edit
-Cart View: /merchstore/cart
-Transactions List View: /merchstore/transactions
-
-"""
