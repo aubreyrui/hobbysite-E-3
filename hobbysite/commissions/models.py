79fd2a70488df89d2b82a502c43e9f51ad17c398
Giuliana <giuliana.bautista@student.ateneo.edu>
2024-05-05 11:06:24 +0800

    fixed the sorting of commissions model

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -2,21 +2,23 @@ from datetime import datetime
 from django.db import models
 from django.urls import reverse
 from user_management.models import Profile
+from django.utils.translation import gettext_lazy as _
 
 
 class Commission(models.Model):
-    STATUS_CHOICES = { # https://stackoverflow.com/questions/18676156/how-to-properly-use-the-choices-field-option-in-django
-        "OPEN": "Open",
-        "FULL": "Full",
-        "COMPLETED": "Completed",
-        "DISCONTINUED": "Discontinued",
-    }
+    
+    class StatusChoices(models.IntegerChoices):
+        OPEN = 1, "Open"
+        FULL = 2, "Full"
+        COMPLETED = 3, "Completed"
+        DISCONTINUED = 4, "Discontinued"
+
+    
     title = models.CharField(max_length = 255)
     description = models.TextField(null = False)
-    status = models.CharField(
-        max_length = 12,
-        choices = STATUS_CHOICES.items(),
-        default = "OPEN",
+    status = models.PositiveSmallIntegerField(
+        choices = StatusChoices.choices,
+        default = StatusChoices.OPEN,
         )
     people_required = models.IntegerField(null = False)
     created_on = models.DateField(null = False, auto_now_add = True)
@@ -24,7 +26,7 @@ class Commission(models.Model):
 
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['status', 'created_on']
     
     
     def __str__(self):
@@ -36,8 +38,8 @@ class Commission(models.Model):
 
 class Job(models.Model):
     STATUS_CHOICES = {
-		"OPEN": "Open",
-		"FULL": "Full",
+		0: "Open",
+		1: "Full",
     }
 
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='commissions')
@@ -46,7 +48,7 @@ class Job(models.Model):
     status = models.CharField(
         max_length = 4,
 		choices = STATUS_CHOICES.items(),
-		default = "OPEN",
+		default = 0,
         )
     
 
