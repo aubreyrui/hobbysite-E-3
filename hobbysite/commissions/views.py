61af2c48700fffb45eb022bba33c8949ceaf32f0
Giuliana <giuliana.bautista@student.ateneo.edu>
2024-05-08 01:34:11 +0800

    ongoing edits for DetailView

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -6,9 +6,11 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.forms import formset_factory
+from django.http import HttpResponseBadRequest
+from django.db.models import Sum
 
 from .models import Commission, Job, JobApplication
-from .forms import CommissionForm, JobForm, JobFormSet ,JobApplicationForm 
+from .forms import CommissionForm, JobFormSet ,JobApplicationForm 
 
 def commissions_list(request):    
     commissions = Commission.objects.all()
@@ -20,7 +22,7 @@ def commissions_list(request):
     return render(request, "commissions_list.html", ctx)
 
 
-def commissions_detail(request, pk):
+def commissions_detail(self, request, pk):
     commission = Commission.objects.get(pk=pk)
     ctx = {"commission": commission}
     return render(request, "commission_detail.html", ctx)
@@ -45,79 +47,85 @@ class CommissionsListView(ListView):
 class CommissionsDetailView(DetailView):
     model = Commission
     template_name = 'commissions_detail.html'
-
-    def post(self, request, *args, **kwargs):
-        application_id = request.POST.get('application_id')
-        job = get_object_or_404(Job, pk=application_id) # https://stackoverflow.com/questions/17813919/django-error-matching-query-does-not-exist
-        accepted_applicants = JobApplication.objects.filter(status='Accepted').count()
-        if accepted_applicants < job.manpower_required + 1:
-            JobApplication.objects.create(job=JobApplication, applicant=request.user.profile)
-            JobApplication.jobapplication_set.create(applicant=request.user.profile, status='Accepted')
-            return redirect('commissions:commission', pk=self.kwargs['pk'])
-        else:
-            pass
-    # # form_class1 = CommissionForm()
-    # form_class = JobForm()
+    form_class = JobApplicationForm
 
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     if self.request.POST:
-    #         context['form'] = self.form_class(self.request.POST)
-    #     else:
-    #         context['form'] = self.form_class()
-    #     return context
-
-    # def form_valid(self, form):
-    #     form.instance.author = self.request.user.profile
-    #     context = self.get_context_data()
-    #     job_formset = context['job_formset']
-    #     if job_formset.is_valid():
-    #         self.object = form.save()
-    #         job_formset.instance = self.object
-    #         job_formset.save()
-    #         return super().form_valid(form)
-    #     else:
-    #         return self.render_to_response(self.get_context_data(form=form))
-
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['form'] = CommissionForm()
-    #     context['commission'] = Commission.objects.all()
-    #     context['job'] = Job.objects.all()
-    #     return context
-    
+    # def get_object(self, queryset=None):
+    #     return super().get_object(queryset=queryset)
 
     # def post(self, request, *args, **kwargs):
-    #     self.object = self.get_object()
-    #     form = CommissionForm(request.POST)
-    #     if 'create_commission' in request.POST:
+    #     commission = self.get_object()
+    #     job_application_form = JobApplicationForm(request.POST)
+    #     if job_application_form.is_valid():
+    #         job_application = job_application_form.save(commit=False)
+    #         job_application.job = commission
+    #         job_application.applicant = request.user.profile 
+    #         job_application.save()
+    #         return redirect('commissions:commissions_detail', pk=commission.pk)
+    #     else:
+    #         return self.render_to_response(self.get_context_data())
 
 
-    #     '''
-    #     if form.is_valid():
-    #         commission = form.save(commit=False)
-    #         commission.title = self.object
-    #         commission.author = self.request.user.profile
-    #         commission.save()
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['job_application_form'] = JobApplicationForm()  # Add the job application form to the context
+    #     return context
+
+    # def post(self, request, *args, **kwargs):
+    #     # commission = self.get_object()  # Retrieve the commission object
+    #     job_application_form = JobApplicationForm(request.POST)
+    #     if job_application_form.is_valid():
+    #         job_application = job_application_form.save(commit=False)
+    #         job_application.job = job_application_form.cleaned_data.get('job')
+    #         job_application.applicant = request.user.profile 
+    #         job_application.save()
+    #         return self.get(request, *args, **kwargs)
+    #         # return redirect('commissions:commissions_detail', pk=commission.pk)
+    #     else:
+    #         context = self.get_context_data(**kwargs)
+    #         context['job_application_form'] = job_application_form
+    #         return self.render_to_response(context)
+    #         # If form is not valid, render the detail view with the form
+    #         return self.render_to_response(self.get_context_data())
+
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     total_manpower_required = Job.objects.aggregate(total_manpower=Sum('manpower_required'))['total_manpower'] or 0
+    #     total_signees = JobApplication.objects.filter(status='Accepted').count()
+    #     open_manpower = max(total_manpower_required - total_signees, 0)
+    #     context['total_manpower_required'] = total_manpower_required
+    #     context['total_signees'] = total_signees
+    #     context['open_manpower'] = open_manpower
+    #     return context
+
+
+    # def post(self, request, *args, **kwargs):
+    #     job_application_form = JobApplicationForm(request.POST)
+    #     # context = self.get_context_data()
+    #     # job_application = context['job_application']
+    #     if job_application_form.is_valid():
+    #         job_application = JobApplication()
+    #         job_application.applicant = self.request.user.profile
+    #         job_application.job = job_application_form.cleaned_data.get('job')
+    #         job_application.save()
     #         return self.get(request, *args, **kwargs)
     #     else:
-    #         self.object_list = self.get_queryset()
+    #         self.object_list = self.get_queryset(**kwargs)
     #         context = self.get_context_data(**kwargs)
-    #         context['form'] = form
+    #         context['job_application_form'] = job_application_form
     #         return self.render_to_response(context)
-    #     '''
 
-
-class CommissionsCreateView(LoginRequiredMixin, CreateView):
-    model = Commission
-    template_name = 'commissions_create.html'
-    form_class = CommissionForm
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user.profile
-        return super().form_valid(form)
+        # commission = self.get_object()
+        # form = JobApplicationForm(request.POST)
+        # if form.is_valid():
+        #     job_application = form.save(commit=False)
+        #     job_application.job = commission
+        #     job_application.applicant = request.user.profile 
+        #     job_application.save()
+        #     return redirect('commissions:commissions_detail', pk=commission.pk)
+        # else:
+        #     return self.render_to_response(self.get_context_data(form=form))
 
 
 class CommissionsJobCreateView(LoginRequiredMixin, CreateView):
@@ -152,7 +160,7 @@ class CommissionsJobCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
        
 
-class CommissionsUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+class CommissionsUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Commission
     template_name = 'commissions_update.html'
     form_class = CommissionForm
@@ -160,3 +168,5 @@ class CommissionsUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
     def test_func(self):
         obj = self.request.user.profile
         return obj.user == self.request.user
+    
+    # if model.job.status == 'Full':
