c6b6e41b69aa7e4d9c97e41ddd168ae9e7483e9c
kristine-banzon <kristine.banzon@student.ateneo.edu>
2024-05-08 14:58:03 +0800

    corrected the extended html files

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -6,8 +6,8 @@ from .views import ArticleDetailView, ArticleListView, ArticleCreateView, Articl
 urlpatterns = [
     path('articles', ArticleListView.as_view(), name='blog'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='blog_detail'),
-    path('article/add', ArticleCreateView.as_view(), name= 'blog_write'),
-    path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name = 'blog_edit')
+    path('article/add', ArticleCreateView.as_view(), name='blog_write'),
+    path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name='blog_edit')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -43,9 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'merchstore',
-    'wiki',
-    'commissions',
     'blog',
     'user_management'
 ]
@@ -126,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static',]
 LOGIN_REDIRECT_URL = 'landing_page'
 LOGOUT_REDIRECT_URL = '/profile/login'
